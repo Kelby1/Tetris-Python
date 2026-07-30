e5fc4f4985f12b4a9de04ee38bbf806dfe71bf99
Kelby Dati <kelbydati1@gmail.com>
2026-07-30 10:59:56 +0800

    Added falldown and Speed fall

--- main.py
+++ main.py
@@ -17,17 +17,24 @@ piece = Piece()
 
 running = True
 
+fall_time = 0
+fall_speed = 500
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type ==pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key== pygame.K_LEFT:
                 piece.move_left()
             elif event.key == pygame.K_RIGHT:
                 piece.move_right()
             elif event.key == pygame.K_DOWN:
                 piece.move_down()
+    fall_time += clock.get_time()
+    if fall_time >= fall_speed:
+        piece.move_down()
+        fall_time = 0
 
     screen.fill((30,30,30))
     board.draw(screen)
